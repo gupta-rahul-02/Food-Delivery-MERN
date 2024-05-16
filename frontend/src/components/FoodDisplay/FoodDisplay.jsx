94a7e4a73e318af0e71431ee6453c827904ab085
rahul gupta <rahulguptaslg20@gmail.com>
2024-05-16 19:17:51 +0530

    Home page completed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,8 +9,10 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishses near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index) =>{
-                console.log(item);
-                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                if(category ==="All" || category===item.category){
+                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+
+                }
             })}
         </div>
     </div>
